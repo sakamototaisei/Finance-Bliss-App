c16e58f7e4441f408d4ae9f75a6c2c1fa665cf77
sakamototaisei <taisei0403089@gmail.com>
2021-07-08 14:42:43 +0000

    add post_new_index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,23 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
-  
-  def create
-    
 
   def edit
   end
@@ -19,6 +26,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:introduction, :image)
   end
 
 end
