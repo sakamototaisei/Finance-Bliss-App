92b5548fd318ec5d76c947c597f3d31394d19063
sakamototaisei <taisei0403089@gmail.com>
2021-07-10 11:09:11 +0000

    add search controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,14 @@ class LikesController < ApplicationController
     post = Post.find(params[:post_id])
     like = current_user.likes.new(post_id: post.id)
     like.save
-    redirect_to post_path(post)
+    redirect_to request.referer
   end
 
   def destroy
     post = Post.find(params[:post_id])
     like = current_user.likes.find_by(post_id: post.id)
     like.destroy
-    redirect_to post_path(post)
+    redirect_to request.referer
   end
 
 end
