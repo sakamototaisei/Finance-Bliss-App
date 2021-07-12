0ff9fea0cabfb101b35cc3a0462c586792076eb3
sakamototaisei <taisei0403089@gmail.com>
2021-07-12 14:54:27 +0000

    add Ajax comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,15 +2,16 @@ class PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = @post.id
-    comment.save
-    redirect_to post_path(post)
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    @post_comment.user_id = current_user.id
+    @post_comment.save
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    post_comment = @post.post_comments.find(params[:id])
+    post_comment.destroy
   end
 
   private
