1aa6b736b9bf85874668dd926e6fd628af5a1702
sakamototaisei <taisei0403089@gmail.com>
2021-07-13 15:23:17 +0000

    add view deposit nisa

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  get "answer", to: "homes#answer"
+  post "answer", to: "homes#answer"
   get "about", to: "homes#about"
   get "save", to: "homes#save"
   get "investment", to: "homes#investment"
