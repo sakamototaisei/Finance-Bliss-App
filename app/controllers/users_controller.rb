c3f39caad1905eead9740f6be2b6086368f455f2
sakamototaisei <taisei0403089@gmail.com>
2021-10-17 15:02:18 +0000

    ゲスト機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    user = User.find_by(name: "ゲスト")
+    if user.name == "ゲスト"
+      redirect_to user_path(current_user)
+    end
     if @user.id != current_user.id
       redirect_to user_path(current_user)
     end
@@ -33,6 +37,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @likes = Like.where(user_id: @user.id)
     @genres = Genre.all
+  end
+
+  # ゲストログイン
+  def new_guest
+    # emailでユーザーが見つからなければ作ってくれるという便利なメソッド
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+    # 自分はユーザー登録時にニックネームを必須にしているのでこの記述が必要
+    user.name = "ゲスト"
+    # 英数字混合を必須にしているので、ランダムパスワードに、英字と数字を追加してバリデーションに引っかからないようにしています。
+    user.password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
+    end
+    # sign_inはログイン状態にするデバイスのメソッド
+    sign_in user
+    # ログイン後root_pathに飛ぶようにしました。
+    redirect_to users_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/users/guest_sign_in', to: 'users#new_guest'
+
   root to: "homes#top"
   post "answer", to: "homes#answer"
   get "about", to: "homes#about"
